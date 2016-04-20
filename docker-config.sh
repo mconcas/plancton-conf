55a22b3b23d78bc3d62ee44cd1ab874ca6b6ccf0
Matteo Concas <mconcas@cern.ch>
2016-04-20 12:44:00 +0200

    - Created docker-config.sh + DNS configuration
    More post-installation, environment-dependent, Docker configurations should be placed here.

---
+++ docker-config.sh
@@ -0,0 +1,13 @@
+#! /bin/bash -x
+
+if [[ -f /etc/default/docker ]]; then
+  echo "Setting DNS to 192.84.137.2"
+  echo "DOCKER_OPTS="--dns 192.84.137.2 --dns 8.8.8.8 --dns 8.8.4.4"" >> /etc/default/docker
+else
+  echo "File not found, perhaps on a not ubuntu-based distro..."
+fi
+
+echo "restarting docker daemon..."
+service docker stop > /dev/null 2>&1
+[[ ! service docker start ]] && echo "couldn't, please start/restart it manually..."
+
