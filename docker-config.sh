f11f33905cbfabefabea04eac6057230f4024328
Matteo Concas <mconcas@cern.ch>
2016-04-20 15:13:32 +0200

    - Added fix due to different Docker default names
    I found that in Docker 1.0.1 in /etc is located a file /etc/default/docker.io instead /etc/default/docker

--- docker-config.sh
+++ docker-config.sh
@@ -6,7 +6,12 @@ if [[ -f /etc/default/docker ]]; then
 else
   echo "File not found, perhaps on a not ubuntu-based distro..."
 fi
-
+if [[ -f /etc/default/docker.io ]]; then
+  echo "Setting DNS to 192.84.137.2"
+  sed -i -e 's/# DOCKER_OPTS="--dns 8.8.8.8 --dns 8.8.4.4"/DOCKER_OPTS="--dns 192.84.137.2 --dns 8.8.8.8 --dns 8.8.4.4"/g' /etc/default/docker.io
+else
+  echo "File not found, perhaps on a not ubuntu-based distro..."
+fi
 echo "restarting docker daemon..."
 service docker stop > /dev/null 2>&1
 if ! service docker start; then
