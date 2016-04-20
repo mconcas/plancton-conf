e0458e40a66785882efc033380c246f9e747637a
Matteo Concas <mconcas@cern.ch>
2016-04-20 14:55:41 +0200

    - Now using inline substitution

--- docker-config.sh
+++ docker-config.sh
@@ -2,12 +2,13 @@
 
 if [[ -f /etc/default/docker ]]; then
   echo "Setting DNS to 192.84.137.2"
-  echo "DOCKER_OPTS="--dns 192.84.137.2 --dns 8.8.8.8 --dns 8.8.4.4"" >> /etc/default/docker
+  sed -i -e 's/# DOCKER_OPTS="--dns 8.8.8.8 --dns 8.8.4.4"/DOCKER_OPTS="--dns 192.84.137.2 --dns 8.8.8.8 --dns 8.8.4.4"/g' /etc/default/docker
 else
   echo "File not found, perhaps on a not ubuntu-based distro..."
 fi
 
 echo "restarting docker daemon..."
 service docker stop > /dev/null 2>&1
-[[ ! service docker start ]] && echo "couldn't, please start/restart it manually..."
-
+if ! service docker start; then
+  echo "couldn't, please start/restart it manually..."
+fi
