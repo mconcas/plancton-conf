99dec586a2e56fb1494a6b5b84281987981b80fa
Matteo Concas <mconcas@cern.ch>
2016-04-21 11:17:26 +0200

    Update docker-config.sh

--- docker-config.sh
+++ docker-config.sh
@@ -8,7 +8,7 @@ else
 fi
 if [[ -f /etc/default/docker.io ]]; then
   echo "Setting DNS to 192.84.137.2"
-  sed -i -e 's/# DOCKER_OPTS="--dns 8.8.8.8 --dns 8.8.4.4"/DOCKER_OPTS="--dns 192.84.137.2 --dns 8.8.8.8 --dns 8.8.4.4"/g' /etc/default/docker.io
+  sed -i -e 's/#DOCKER_OPTS="--dns 8.8.8.8 --dns 8.8.4.4"/DOCKER_OPTS="--dns 192.84.137.2 --dns 8.8.8.8 --dns 8.8.4.4"/g' /etc/default/docker.io
 else
   echo "File not found, perhaps on a not ubuntu-based distro..."
 fi
